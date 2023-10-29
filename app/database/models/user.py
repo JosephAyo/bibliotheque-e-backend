97833dfb37b16f811f08374171bb9a31325e1367
Ayo Joseph <josephayo110@gmail.com>
2023-10-29 19:16:33 +0100

    🗃️ feat: add user db model

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,10 +1,38 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import TIMESTAMP, Boolean, Column, Enum, Integer, String, Text, func
 from ..base import Base
+import uuid
+
+enum_suspension_status = Enum(
+    "admin_action",
+    "password_attempt",
+    "terms_of_service_violation",
+    name="enum_suspension_status",
+)
 
 
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
+    id = Column(
+        "id", Text(length=36), default=lambda: str(uuid.uuid4()), primary_key=True
+    )
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(Text)
+    is_email_verified = Column(Boolean)
+    is_verified = Column(Boolean)
+    country = Column(Text)
+    suspension_status = Column(enum_suspension_status)
+    is_deactivated = Column(Boolean, nullable=False, default=False)
+    incorrect_password_attempt_count = Column(Integer)
+    otp = Column(Text)
+    verification_code = Column(Text)
+    reset_password_code = Column(Text)
+    verification_code_last_generated_at = Column(TIMESTAMP)
+    reset_password_code_last_generated_at = Column(TIMESTAMP)
+    last_login = Column(TIMESTAMP)
+    password_reset_at = Column(TIMESTAMP)
+    created_at = Column(TIMESTAMP, server_default=func.now(), nullable=False)
+    updated_at = Column(TIMESTAMP, server_default=func.now(), nullable=False)
+    deleted_at = Column(TIMESTAMP)
+    deactivated_at = Column(TIMESTAMP)
