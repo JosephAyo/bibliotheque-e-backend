87b90884d79bed94c2929ffd0c8efdafcd799646
Ayo Joseph <josephayo110@gmail.com>
2023-10-29 18:42:04 +0100

    🔧 🗃️ feat: setup db connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .routers import user, library
+from .database.base import engine
+from .database.models import user as user_model
 
 app = FastAPI()
+user_model.Base.metadata.create_all(engine)
 
 
 app.include_router(library.router)
