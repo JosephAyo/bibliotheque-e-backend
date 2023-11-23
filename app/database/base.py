406bdec2f3a46f1b2e8ec9fd1cd5ac5d3ce93fdf
Ayo Joseph <josephayo110@gmail.com>
2023-11-23 06:14:16 +0100

    🐛 fix: fix deployment failing by increasing db pool config

--- app/database/base.py
+++ app/database/base.py
@@ -3,7 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE = "sqlite:///./bibliotheque-e.db"
-engine = create_engine(SQLALCHEMY_DATABASE, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE,
+    connect_args={"check_same_thread": False},
+    pool_size=20,
+    max_overflow=10,
+    pool_timeout=80,
+)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
